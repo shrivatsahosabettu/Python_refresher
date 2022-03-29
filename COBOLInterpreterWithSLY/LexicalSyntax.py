3eb280be4dabcfa0af23ff362c7f39467eee8b93
shrivatsahosabettu <shrivatsa555@gmail.com>
2022-03-29 23:53:25 +0530

    Programming with SLY

--- COBOLInterpreterWithSLY/LexicalSyntax.py
+++ COBOLInterpreterWithSLY/LexicalSyntax.py
@@ -3,12 +3,11 @@ from sly import Lexer
 
 class CobLexer(Lexer):
 
-    tokens = {COBOL_WORD, ALPHA_USER_DEFINED_WORDS, COBOL_WORD, INTEGER, LEVEL_NUMBER,
-              DISPLAY, STOP, RUN, TEXT}
+    tokens = {COBOL_WORD, ALPHA_USER_DEFINED_WORDS, INTEGER, LEVEL_NUMBER,
+              DISPLAY, STOP, RUN, TEXT, ASSIGN, PLUS, MINUS, TIMES, DIVIDE, DOT}
 
     # Literal characters to operator precedence override
     literals = {'(', ')','.',"'",'"'}
-
 
     # String containing ignored characters between tokens
     ignore = ' \t'
@@ -16,19 +15,13 @@ class CobLexer(Lexer):
     # Other ignored patterns
     ignore_newline = r'\n+'
 
-    '''
-    A COBOL word is a character-string of not more than 30 characters which
-    forms a user-defined word, a system-name, or a reserved word. Except for
-    arithmetic operators and relation characters, each character of a   COBOL
-    word is selected from the set of letters, digits, and the hyphen; the hyphen 
-    cannot appear as the first or last character in such words. Each lowercase 
-    letter is considered to be equivalent to its corresponding uppercase letter.
-    '''
+    ASSIGN = r'='
+    PLUS = r'\+'
+    MINUS = r'-'
+    TIMES = r'\*'
+    DIVIDE = r'/'
+    DOT = r'\.'
 
-    COBOL_WORD = r'[A-Za-z0-9]+([\-]+[A-Za-z0-9]+)*'
-
-    # User defined words
-    ALPHA_USER_DEFINED_WORDS = r'([0-9]+[\-]*)*[0-9]*[A-Za-z][A-Za-z0-9]*([\-]+[A-Za-z0-9]+)*'
 
     TEXT = r'~("\n" | "\r")'
 
@@ -41,6 +34,7 @@ class CobLexer(Lexer):
     '''
 
     INTEGER = r'[0]*[1-9][0-9]*'
+
     @_(r'[0]*[1-9][0-9]*')
     def INTEGER(self, t):
         t.value = int(t.value)
@@ -52,10 +46,23 @@ class CobLexer(Lexer):
 
     LEVEL_NUMBER = r'([0]?[1-9]|[1-4][0-9]|"66"|"77"|"88")'
 
+    '''
+     A COBOL word is a character-string of not more than 30 characters which
+     forms a user-defined word, a system-name, or a reserved word. Except for
+     arithmetic operators and relation characters, each character of a   COBOL
+     word is selected from the set of letters, digits, and the hyphen; the hyphen 
+     cannot appear as the first or last character in such words. Each lowercase 
+     letter is considered to be equivalent to its corresponding uppercase letter.
+     '''
+
+    COBOL_WORD = r'[A-Za-z0-9]+([\-]+[A-Za-z0-9]+)*'
     # Identifiers and keywords
     COBOL_WORD['DISPLAY', 'display'] = DISPLAY
     COBOL_WORD['STOP', 'stop'] = STOP
     COBOL_WORD['RUN', 'run'] = RUN
+
+    # User defined words
+    ALPHA_USER_DEFINED_WORDS = r'([0-9]+[\-]*)*[0-9]*[A-Za-z][A-Za-z0-9]*([\-]+[A-Za-z0-9]+)*'
 
     @_(r'\n+')
     def ignore_newline(self, t):
@@ -70,8 +77,12 @@ if __name__ == '__main__':
     data = '''
        ID-DIVISION. 
        PROGRAM-ID. TEST01.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-RESULT                      PIC 9(05).
        PROCEDURE DIVISION.
            DISPLAY 'HELLO WORLD'.
+           COMPUTE WS-RESULT = 3 + 5 * 6.
            STOP RUN.
 '''
     lexer = CobLexer()
